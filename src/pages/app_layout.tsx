c2911a0acd731473b7f389a842bca365fd631bb6
thesayf <r_ori@hotmail.co.uk>
2023-09-29 14:52:50 +0100

    commented out Logo component to see if it just this componet wit the file dependecy issues or if it will all the components.

--- src/pages/app_layout.tsx
+++ src/pages/app_layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../components/logo"
+// import Logo from "../components/Logo"
 import AppName from "../components/AppName"
 import DATEHEADER from "../headers/DateHeader"
 import MAINGOALHEADER from "../headers/MainGoalHeader";
@@ -20,7 +20,7 @@ const AppLayout: React.FC = () => {
 
             {/* Logo and App Name */}
             <div className="grid grid-cols-1 md:grid-cols-2 h-20">
-                <Logo/>
+                {/* <Logo/> */}
                 <AppName/>
             </div>
 
