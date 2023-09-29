0e392c7892fcfdf3a8c58c211a6c3d26f25a1505
thesayf <r_ori@hotmail.co.uk>
2023-09-29 15:48:26 +0100

    Trying to fix netlify deplyment issue where it cant find modules

--- src/pages/test_layout.tsx
+++ src/pages/test_layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TestComponent from "../components/test_component";
-import Logo from "../components/logo_component"
+import Logo from "../components/logo"
 
 const TestLayout: React.FC = () => { 
 
