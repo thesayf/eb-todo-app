4c97ae7f1becf8b224fcd9af2e861ec258339e28
thesayf <r_ori@hotmail.co.uk>
2023-09-29 15:04:34 +0100

    Trying to fix netlify deplyment issue where it cant find modules

--- src/pages/app_layout.tsx
+++ src/pages/app_layout.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-// import Logo from "../components/Logo"
-import AppName from "../components/AppName"
-import DATEHEADER from "../headers/DateHeader"
-import MAINGOALHEADER from "../headers/MainGoalHeader";
-import MainGoal from "../components/MainGoal";
-import DateComponent from "../components/Date";
-import KeyObjectivesHeader from "../headers/KeyObjectivesHeader";
-import ObjectiveOne from "../components/ObjectiveOne";
-import ObjectiveTwo from "../components/ObjectiveTwo";
-import ObjectiveThree from "../components/ObjectiveThree";
-import ObjectiveFour from "../components/ObjectiveFour";
-import ScheduleHeader from "../headers/ScheduleHeader";
-import TimeSlot from "../components/TimeSlot";
-import TimeSlotAdder from "../components/TimeSlotAdder";
+import Logo from '../components/Logo'
+// import AppName from "../components/AppName"
+// import DATEHEADER from "../headers/DateHeader"
+// import MAINGOALHEADER from "../headers/MainGoalHeader";
+// import MainGoal from "../components/MainGoal";
+// import DateComponent from "../components/Date";
+// import KeyObjectivesHeader from "../headers/KeyObjectivesHeader";
+// import ObjectiveOne from "../components/ObjectiveOne";
+// import ObjectiveTwo from "../components/ObjectiveTwo";
+// import ObjectiveThree from "../components/ObjectiveThree";
+// import ObjectiveFour from "../components/ObjectiveFour";
+// import ScheduleHeader from "../headers/ScheduleHeader";
+// import TimeSlot from "../components/TimeSlot";
+// import TimeSlotAdder from "../components/TimeSlotAdder";
 
 const AppLayout: React.FC = () => {
     return(
@@ -20,44 +20,44 @@ const AppLayout: React.FC = () => {
 
             {/* Logo and App Name */}
             <div className="grid grid-cols-1 md:grid-cols-2 h-20">
-                {/* <Logo/> */}
-                <AppName/>
+                <Logo/>
+                {/* <AppName/> */}
             </div>
 
             {/* Date and Main Goal Headers */}
             <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mt-2">
-                <DATEHEADER/>
-                <MAINGOALHEADER/>
+                {/* <DATEHEADER/>
+                <MAINGOALHEADER/> */}
             </div>
 
             {/* Date and Main Goal */}
             <div className="grid grid-cols-1 md:grid-cols-4 gap-4 h-16 mt-2">
-                <DateComponent/>
-                <MainGoal/>
+                {/* <DateComponent/>
+                <MainGoal/> */}
             </div>
 
             {/* Header For Key Objectives */}
             <div className="grid grid-cols-1 mt-2 ">
-                <KeyObjectivesHeader/>
+                {/* <KeyObjectivesHeader/> */}
             </div>
 
             {/* 4 Sub Objectives */}
             <div className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 gap-4 mt-2">                
-                <ObjectiveOne/>
+                {/* <ObjectiveOne/>
                 <ObjectiveTwo/>
                 <ObjectiveThree/>
-                <ObjectiveFour/>    
+                <ObjectiveFour/>     */}
             </div>
 
             {/* Header For Todays Schedule */}
             <div className="grid grid-cols-1 mt-2">
-                <ScheduleHeader/>
+                {/* <ScheduleHeader/> */}
             </div>
 
             {/* Time Table */}
             <div className="grid grid-cols-1 gap-4 mt-2">
-                <TimeSlot/>
-                <TimeSlotAdder/>
+                {/* <TimeSlot/>
+                <TimeSlotAdder/> */}
             </div>
 
         </div>
