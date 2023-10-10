95c417dc2694449ad96ea2a79604e742d775d1d1
thesayf <r_ori@hotmail.co.uk>
2023-10-10 19:16:12 +0100

    Started adding state to inputs

--- src/components/main_goal_component.tsx
+++ src/components/main_goal_component.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useAppContext } from "../app_context";
 
 const MainGoal: React.FC = () => {
+    const { mainGoal, setMainGoal } = useAppContext();
+
     return(
         <div className="col-span-full md:col-span-4 h-full">
-            <input className="w-full input input-lg input-bordered text-xl" type="text" placeholder="Enter your main goal" />
+            <input 
+                className="w-full input input-lg input-bordered text-xl" 
+                type="text" 
+                placeholder="Enter your main goal"
+                value={mainGoal}
+                onChange={(e) => setMainGoal(e.target.value)}
+                 />
         </div>
     )
 }
